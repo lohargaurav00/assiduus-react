5cc8ceed0313c8e7604b943aa19a351e7eefebf9
Gaurav Lohar <lohargaurav00@gmail.com>
2023-11-07 20:17:29 +0530

    + Sidebar

---
+++ Components/Sidebar.tsx
@@ -0,0 +1,93 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import { IconButton, ListItemText, Toolbar, Typography } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+import { paths } from '@/utils/paths';
+
+const drawerWidth = 240;
+
+export default function PermanentDrawerLeft() {
+  const [active, setActive] = React.useState<string>('Dashboard');
+
+    
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box'
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar className='mb-6'>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="Github"
+          >
+            <GitHubIcon fontSize='large' />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{
+              flexGrow: 1,
+              fontWeight: 'bold',
+              fontSize: '1.5rem'
+            }}
+          >
+            Gaurav
+          </Typography>
+        </Toolbar>
+        <List >
+          {paths.map((item, index) => (
+            <ListItem key={index} disablePadding  >
+              <ListItemButton onClick={() => setActive(() => item.name)} sx={{
+                backgroundColor: active === item.name ? 'var(--green-primary)' : 'transparent',
+                '&:hover': {
+                  backgroundColor: active === item.name ? 'var(--green-primary)' : 'transparent',
+                },
+              }}>
+                <ListItemIcon sx={{
+                  paddingLeft: '20px',
+
+                  '& .MuiSvgIcon-root': {
+                    color: active === item.name ? 'white' : 'var(--text-primary)',
+                    width: '18px',
+                    height: '18px',
+                  }
+                }
+                }>
+                  <item.icon />
+                </ListItemIcon>
+                <ListItemText primary={item.name} sx={
+                  {
+                    '& .MuiListItemText-primary': {
+                      color: active === item.name ? 'white' : 'var(--text-primary)',
+                      fontWeight: 'bold',
+                      fontSize: '14px'
+                    }
+                  }
+                } />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+}
