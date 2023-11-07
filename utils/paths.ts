826b597cf19303e466591035990b4e9430077bb4
Gaurav Lohar <lohargaurav00@gmail.com>
2023-11-07 19:33:25 +0530

    + paths

---
+++ utils/paths.ts
@@ -0,0 +1,39 @@
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import DescriptionIcon from '@mui/icons-material/Description';
+import PersonIcon from '@mui/icons-material/Person';
+import ContactsIcon from '@mui/icons-material/Contacts';
+
+export const paths = [
+    {
+        path: '/',
+        name: 'Dashboard',
+        icon: DashboardIcon,
+    },
+    {
+        path: '/',
+        name: 'Accounts',
+        icon: AccountBalanceWalletIcon,
+    },
+    {
+        path: '/',
+        name: 'Payroll',
+        icon: AttachMoneyIcon,
+    },
+    {
+        path: '/',
+        name: 'Reports',
+        icon: DescriptionIcon,
+    },
+    {
+        path: '/',
+        name: 'Employees',
+        icon: PersonIcon,
+    },
+    {
+        path: '/',
+        name: 'Contacts',
+        icon: ContactsIcon,
+    },
+]
