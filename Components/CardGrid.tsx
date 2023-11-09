6717cd703a0e734e88debaa6cf2656b263fff4a2
Gaurav Lohar <lohargaurav00@gmail.com>
2023-11-09 20:37:22 +0530

    linechart

--- Components/CardGrid.tsx
+++ Components/CardGrid.tsx
@@ -1,34 +1,35 @@
-import React from 'react'
+"use client";
+import React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Unstable_Grid2";
-import BasicCard from "@/components/BasicCard";
+import LinechartCard from "./LinechartCard";
 
 const CardGrid = () => {
   return (
     <Box
-    sx={{
-      flexGrow: 1,
-      margin: "40px 40px 40px 280px",
-      height: "calc(100vh - 126px)",
-      maxWidth: "100%",
-    }}
-  >
-    <Grid container spacing={4} height={"100%"}>
-      <Grid xs={6}>
-        <BasicCard />
+      sx={{
+        flexGrow: 1,
+        margin: "40px 40px 40px 280px",
+        height: "calc(100vh - 126px)",
+        maxWidth: "100%",
+      }}
+    >
+      <Grid container spacing={4} height={"100%"}>
+        <Grid xs={6}>
+          <LinechartCard />
+        </Grid>
+        <Grid xs={6}>
+          <LinechartCard />
+        </Grid>
+        <Grid xs={6}>
+          <LinechartCard />
+        </Grid>
+        <Grid xs={6}>
+          <LinechartCard />
+        </Grid>
       </Grid>
-      <Grid xs={6}>
-        <BasicCard />
-      </Grid>
-      <Grid xs={6}>
-        <BasicCard />
-      </Grid>
-      <Grid xs={6}>
-        <BasicCard />
-      </Grid>
-    </Grid>
-  </Box>
-  )
-}
+    </Box>
+  );
+};
 
-export default CardGrid
+export default CardGrid;
