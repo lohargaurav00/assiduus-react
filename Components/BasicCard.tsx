90fdbbaa1c9a82adff0c20164226e506cbf0df83
Gaurav Lohar <lohargaurav00@gmail.com>
2023-11-09 20:35:28 +0530

    + @BasicCard  -> generic

--- Components/BasicCard.tsx
+++ Components/BasicCard.tsx
@@ -1,42 +1,34 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+import {
+  Card,
+  CardHeader,
+  Divider,
+  CardContent,
+  styled,
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
-  >
-    •
-  </Box>
-);
+} from "@mui/material";
 
-export default function BasicCard() {
+const StyledCardHeader = styled(CardHeader)(({ theme }) => ({
+  "& .MuiCardHeader-title": {
+    fontSize: "1rem",
+    fontWeight: 700,
+  },
+}));
+
+type BasicCardProps = {
+  content: React.ReactNode;
+  title: string;
+  action?: React.ReactNode;
+};
+
+const BasicCard: React.FC<BasicCardProps> = ({ content, title, action }) => {
   return (
-    <Card sx={{ minWidth: 275 , height: '100%' }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+    <Card sx={{ minWidth: 275, height: "100%" }}>
+      <StyledCardHeader title={title} action={action} />
+      <Divider />
+      <CardContent >{content}</CardContent>
     </Card>
   );
-}
+};
+
+export default BasicCard;
