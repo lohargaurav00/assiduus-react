42da9dc12e5fae308a011114723ce0236cb5c930
Gaurav Lohar <lohargaurav00@gmail.com>
2023-11-12 15:46:31 +0530

    + deploy test -1

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import Navbar from "@/components/Navbar";
+import Navbar from "../components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import CardGrid from "@/components/CardGrid";
 
