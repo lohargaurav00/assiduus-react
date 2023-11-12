efa904456050edfaf1f03112755264d557a6aa85
Gaurav Lohar <lohargaurav00@gmail.com>
2023-11-12 16:40:25 +0530

    "Remove duplicate file"

--- Components/AccountTableCard.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import BasicCard from "./BasicCard";
-import AccountTable from "./charts/AccountTable";
-
-const AccountTableCard = () => {
-  return <BasicCard title="Account Watchlist" content={<AccountTable/>} />;
-};
-
-export default AccountTableCard;
