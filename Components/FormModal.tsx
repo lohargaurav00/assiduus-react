154f0f83fb144f0416e41a61df9971e7e5fab2bf
Gaurav Lohar <lohargaurav00@gmail.com>
2023-11-12 12:51:18 +0530

    + form

--- Components/FormModal.tsx
+++ Components/FormModal.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+
+import Form from "./From";
 
 const style = {
   position: "absolute",
@@ -11,7 +12,8 @@ const style = {
   transform: "translate(-50%, -50%)",
   width: 400,
   bgcolor: "background.paper",
-  border: "2px solid #000",
+  border: "2px solid var(--text-tertiary)",
+  borderRadius: "10px",
   boxShadow: 24,
   p: 4,
 };
@@ -31,12 +33,7 @@ const FormModal = () => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+        <Form handleClose={handleClose}/>
         </Box>
       </Modal>
     </div>
